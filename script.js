61bda9bcd30dbecb16986aa1a3c51f37881bc150
AlexBlejan <alexandrublejan@yahoo.com>
2024-03-04 19:14:31 +0200

    add adition example

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let age = 33;
 console.log(age);
 
+const piNumber = 3.14;
+console.log(piNumber);
+
 let name = `Alex`;
 console.log(name);
 
 name = `Alex Blejan`;
 console.log(name);
 console.log(`Alex `);
+
+// operatori date numerice
+
+let sum = 5 + 7;
+console.log(sum);
+
+let firstNumber = 12;
+firstNumber = 13;
+let secondNumber = 7;
+let sumNumber = firstNumber + secondNumber;
+console.log(sumNumber);
+firstNumber = 50;
+sumNumber = firstNumber + secondNumber;
+console.log(sumNumber);
+console.log(`Suma dintre ${firstNumber} si ${secondNumber} este ${sumNumber}`);
